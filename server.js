23e0572a3de8ad995c05e85780bb2a2e04338265
Andres Roldan <dresroldan@gmail.com>
2021-01-05 12:48:57 -0600

    created server.js with necessary dependencies and middleware

---
+++ server.js
@@ -0,0 +1,44 @@
+// required dependencies
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+// allows environment variables in the .env file
+require("dotenv").config();
+
+// creates express server with the port number that the server will be hosted on
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// middleware here
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+
+// allows us to parse JSON
+app.use(express.json());
+
+// serves up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+// mongoose creates connection to mongodb with the required collection
+mongoose
+  .connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", {
+    useNewUrlParser: true,
+  })
+  .then(() => console.log("Connected to MongoDB"));
+
+// Define API routes here
+
+// Send every other request to the React app
+// Define any API routes before this runs
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+// starts the server
+app.listen(PORT, () => {
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
