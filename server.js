0edb387b19c5d8371f5ba190571e7b51d4a3bea2
Andres Roldan <dresroldan@gmail.com>
2021-01-05 20:28:35 -0600

    set up api routes for books and created booksController with CRUD methods

--- server.js
+++ server.js
@@ -30,10 +30,11 @@ mongoose
   })
   .then(() => console.log("Connected to MongoDB"));
 
-// Define API routes here
+// API routes defined here
+const routes = require("./routes");
+app.use(routes);
 
 // Send every other request to the React app
-// Define any API routes before this runs
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
