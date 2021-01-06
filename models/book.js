68bf90243d7b1cbc80e4b7a246a32bca8f2ab415
Andres Roldan <dresroldan@gmail.com>
2021-01-05 20:33:47 -0600

    modified book model schema

--- models/book.js
+++ models/book.js
@@ -7,37 +7,22 @@ const bookSchema = new Schema(
     title: {
       type: String,
       required: true,
-      unique: true,
-      trim: true,
-      minlength: 3,
     },
     authors: {
       type: String,
       required: true,
-      unique: true,
-      trim: true,
-      minlength: 3,
     },
     description: {
       type: String,
       required: true,
-      unique: true,
-      trim: true,
-      minlength: 3,
     },
     image: {
       type: String,
       required: true,
-      unique: true,
-      trim: true,
-      minlength: 3,
     },
     link: {
       type: String,
       required: true,
-      unique: true,
-      trim: true,
-      minlength: 3,
     },
   },
 
